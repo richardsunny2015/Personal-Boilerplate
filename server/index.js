d7c4b749c503d972aa4a33eefc351cb705004f58
Richard Lanchez <richardsunny2015@gmail.com>
2018-08-06 12:58:56 -0400

    feat(server): server has error handling (#8)

--- server/index.js
+++ server/index.js
@@ -5,6 +5,12 @@ const volleyball = require('volleyball');
 
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
-app.use(volleyball)
+app.use(volleyball);
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || 'Internal Server Error');
+});
 
 app.listen(3000, () => console.log('listening on port 3000'));
